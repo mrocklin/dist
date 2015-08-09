863cee12b7f66f3622f66b8d4cbcad199f2bf733
Matthew Rocklin <mrocklin@gmail.com>
2015-08-09 16:21:56 -0700

    rearrange code order

--- dist/worker.py
+++ dist/worker.py
@@ -10,159 +10,38 @@ import zmq
 context = zmq.Context()
 
 
-@asyncio.coroutine
-def dealer_send_recv(loop, addr, data):
-    socket = context.socket(zmq.DEALER)
-    socket.connect(addr)
-    if not isinstance(data, bytes):
-        data = dumps(data)
-    yield From(delay(loop, socket.send, data))
-    result = yield From(delay(loop, socket.recv))
-    socket.close()  # TODO: LRU sockets
-    result = loads(result)
-    raise Return(result)
+class Worker(object):
+    def __init__(self, ip, port, bind_ip,
+                 metadata_addr, loop=None):
+        self.ip = ip
+        self.port = port
+        self.bind_ip = bind_ip
+        self.metadata_addr = metadata_addr
+        self.data = dict()
+        self.loop = loop or asyncio.get_event_loop()
+        self.work_q = asyncio.Queue(loop=self.loop)
+        self.send_q = asyncio.Queue(loop=self.loop)
+        self.outgoing_q = asyncio.Queue(loop=self.loop)
+        self.control_q = asyncio.Queue(loop=self.loop)
+        self.signal_q = asyncio.Queue(loop=self.loop)
 
+        self.status = 'running'
 
-def keys_to_data(o, data):
-    """
+        self.go = asyncio.gather(
+            work(self.work_q, self.send_q, self.data, self.metadata_addr, loop),
+            control(self.control_q, self.work_q, self.send_q, self.data),
+            send(self.send_q, self.outgoing_q, self.signal_q),
+            comm(ip, port, bind_ip, self.signal_q, self.control_q,
+                 self.outgoing_q, loop, context))
 
-    >>> keys_to_data(('x', 'y'), {'x': 1})
-    (1, 'y')
-    >>> keys_to_data({'a': 'x', 'b': 'y'}, {'x': 1})
-    {'a': 1, 'b': 'y'}
-    """
-    if isinstance(o, (tuple, list)):
-        result = []
-        for arg in o:
-            try:
-                result.append(data[arg])
-            except (TypeError, KeyError):
-                result.append(arg)
-        result = type(o)(result)
+    @property
+    def address(self):
+        return 'tcp://%s:%d' % (self.ip, self.port)
 
-    if isinstance(o, dict):
-        result = {}
-        for k, v in o.items():
-            try:
-                result[k] = data[v]
-            except (TypeError, KeyError):
-                result[k] = v
-    return result
-
-
-@asyncio.coroutine
-def get_data(loop, keys, data, metadata_addr, update=False):
-    local = {k: data[k] for k in keys if k in data}
-    missing = [k for k in keys if k not in local]
-
-    while missing:  # Are we missing anything?
-        # Ask who has the keys we want
-        msg = {'op': 'who-has', 'keys': missing}
-        who_has = yield From(dealer_send_recv(loop, metadata_addr, msg))
-        print("Collecting %s" % who_has)
-
-        # get those keys from remote sources
-        coroutines = [dealer_send_recv(loop, random.choice(list(who_has[k])),
-                                       {'op': 'get-data', 'keys': [k]})
-                                    for k in missing]
-        other = yield From(asyncio.gather(*coroutines))
-
-        # Merge in to local and make sure we aren't still missing anything
-        local.update(merge(other))
-        missing = [k for k in keys if k not in local]
-
-    if update:
-        data.update(local)
-
-    raise Return(local)
-
-
-@asyncio.coroutine
-def work(work_q, send_q, data, metadata_addr, loop=None):
-    """ Work coroutine
-
-    Input Channels:
-        work_q: Main mailbox, get work requests from control
-        metadata_addr: This directly communicates with the MDStore via ZMQ
-        data: A dictionary of local data.  This manages some state.
-
-    Output Channels:
-        send_q: Send acknowledgements of task finished (or failed) to requeter
-        data:  A (possibly modified) dictionary of local data.
-    """
-    print("Worker boots up")
-    loop = loop or asyncio.get_event_loop()
-    while True:
-        addr, msg = yield From(work_q.get())
-        if msg == 'close':
-            break
-
-        key, func, args, kwargs, needed = \
-                get(['key', 'function', 'args', 'kwargs', 'needed'], msg)
-
-        d = yield From(get_data(loop, needed, data, metadata_addr))
-
-        args2 = keys_to_data(args, d)
-        kwargs2 = keys_to_data(kwargs, d)
-
-        # result = yield From(delay(loop, func, *args2, **kwargs2))
-        result = func(*args2, **kwargs2)
-
-        data[key] = result
-
-        out = {'op': 'computation-finished',
-               'key': msg['key']}
-
-        send_q.put_nowait((addr, out))
-
-    raise Return("Done working")
-
-
-@asyncio.coroutine
-def control(control_q, work_q, send_q, data):
-    """ Control coroutine, general dispatching
-
-    Input Channels:
-        control_q: Mailbox for any messages that come in from comm
-
-    Output Channels:
-        work_q: jobs for the worker
-        send_q: people ask us to send them data
-    """
-    print("Control boots up")
-    while True:
-        addr, msg = yield From(control_q.get())
-        if msg == b'close':
-            work_q.put_nowait((addr, msg))
-            send_q.put_nowait((addr, msg))
-            break
-        elif msg['op'] == 'compute':
-            work_q.put_nowait((addr, msg))
-        elif msg['op'] == 'get-data':
-            data = {k: data[k] for k in msg['keys']
-                                if k in data}
-            send_q.put_nowait((addr, data))
-        elif msg['op'] == 'ping':
-            send_q.put_nowait((addr, b'pong'))
-        else:
-            raise NotImplementedError("Bad Message: %s" % msg)
-    raise Return("Done listening")
-
-
-@asyncio.coroutine
-def send(send_q, outgoing_q, signal_q):
-    print("Send boots up")
-    while True:
-        addr, msg = yield From(send_q.get())
-        if msg == b'close':
-            break
-
-        if not isinstance(msg, bytes):
-            msg = dumps(msg)
-        outgoing_q.put_nowait((addr, msg))
-        signal_q.put_nowait('interrupt')
-
-    raise Return("Done sending")
+    @asyncio.coroutine
+    def close(self):
+        self.signal_q.put_nowait(b'close')
+        self.status = 'closing'
 
 
 @asyncio.coroutine
@@ -228,35 +107,156 @@ def comm(ip, port, bind_ip, signal_q, control_q, outgoing_q, loop=None,
     raise Return("Done communicating")
 
 
-class Worker(object):
-    def __init__(self, ip, port, bind_ip,
-                 metadata_addr, loop=None):
-        self.ip = ip
-        self.port = port
-        self.bind_ip = bind_ip
-        self.metadata_addr = metadata_addr
-        self.data = dict()
-        self.loop = loop or asyncio.get_event_loop()
-        self.work_q = asyncio.Queue(loop=self.loop)
-        self.send_q = asyncio.Queue(loop=self.loop)
-        self.outgoing_q = asyncio.Queue(loop=self.loop)
-        self.control_q = asyncio.Queue(loop=self.loop)
-        self.signal_q = asyncio.Queue(loop=self.loop)
+@asyncio.coroutine
+def control(control_q, work_q, send_q, data):
+    """ Control coroutine, general dispatching
 
-        self.status = 'running'
+    Input Channels:
+        control_q: Mailbox for any messages that come in from comm
 
-        self.go = asyncio.gather(
-            work(self.work_q, self.send_q, self.data, self.metadata_addr, loop),
-            control(self.control_q, self.work_q, self.send_q, self.data),
-            send(self.send_q, self.outgoing_q, self.signal_q),
-            comm(ip, port, bind_ip, self.signal_q, self.control_q,
-                 self.outgoing_q, loop, context))
+    Output Channels:
+        work_q: jobs for the worker
+        send_q: people ask us to send them data
+    """
+    print("Control boots up")
+    while True:
+        addr, msg = yield From(control_q.get())
+        if msg == b'close':
+            work_q.put_nowait((addr, msg))
+            send_q.put_nowait((addr, msg))
+            break
+        elif msg['op'] == 'compute':
+            work_q.put_nowait((addr, msg))
+        elif msg['op'] == 'get-data':
+            data = {k: data[k] for k in msg['keys']
+                                if k in data}
+            send_q.put_nowait((addr, data))
+        elif msg['op'] == 'ping':
+            send_q.put_nowait((addr, b'pong'))
+        else:
+            raise NotImplementedError("Bad Message: %s" % msg)
+    raise Return("Done listening")
 
-    @property
-    def address(self):
-        return 'tcp://%s:%d' % (self.ip, self.port)
 
-    @asyncio.coroutine
-    def close(self):
-        self.signal_q.put_nowait(b'close')
-        self.status = 'closing'
+@asyncio.coroutine
+def send(send_q, outgoing_q, signal_q):
+    print("Send boots up")
+    while True:
+        addr, msg = yield From(send_q.get())
+        if msg == b'close':
+            break
+
+        if not isinstance(msg, bytes):
+            msg = dumps(msg)
+        outgoing_q.put_nowait((addr, msg))
+        signal_q.put_nowait('interrupt')
+
+    raise Return("Done sending")
+
+
+@asyncio.coroutine
+def work(work_q, send_q, data, metadata_addr, loop=None):
+    """ Work coroutine
+
+    Input Channels:
+        work_q: Main mailbox, get work requests from control
+        metadata_addr: This directly communicates with the MDStore via ZMQ
+        data: A dictionary of local data.  This manages some state.
+
+    Output Channels:
+        send_q: Send acknowledgements of task finished (or failed) to requeter
+        data:  A (possibly modified) dictionary of local data.
+    """
+    print("Worker boots up")
+    loop = loop or asyncio.get_event_loop()
+    while True:
+        addr, msg = yield From(work_q.get())
+        if msg == 'close':
+            break
+
+        key, func, args, kwargs, needed = \
+                get(['key', 'function', 'args', 'kwargs', 'needed'], msg)
+
+        d = yield From(get_data(loop, needed, data, metadata_addr))
+
+        args2 = keys_to_data(args, d)
+        kwargs2 = keys_to_data(kwargs, d)
+
+        # result = yield From(delay(loop, func, *args2, **kwargs2))
+        result = func(*args2, **kwargs2)
+
+        data[key] = result
+
+        out = {'op': 'computation-finished',
+               'key': msg['key']}
+
+        send_q.put_nowait((addr, out))
+
+    raise Return("Done working")
+
+
+@asyncio.coroutine
+def get_data(loop, keys, data, metadata_addr, update=False):
+    local = {k: data[k] for k in keys if k in data}
+    missing = [k for k in keys if k not in local]
+
+    while missing:  # Are we missing anything?
+        # Ask who has the keys we want
+        msg = {'op': 'who-has', 'keys': missing}
+        who_has = yield From(dealer_send_recv(loop, metadata_addr, msg))
+        print("Collecting %s" % who_has)
+
+        # get those keys from remote sources
+        coroutines = [dealer_send_recv(loop, random.choice(list(who_has[k])),
+                                       {'op': 'get-data', 'keys': [k]})
+                                    for k in missing]
+        other = yield From(asyncio.gather(*coroutines))
+
+        # Merge in to local and make sure we aren't still missing anything
+        local.update(merge(other))
+        missing = [k for k in keys if k not in local]
+
+    if update:
+        data.update(local)
+
+    raise Return(local)
+
+
+@asyncio.coroutine
+def dealer_send_recv(loop, addr, data):
+    socket = context.socket(zmq.DEALER)
+    socket.connect(addr)
+    if not isinstance(data, bytes):
+        data = dumps(data)
+    yield From(delay(loop, socket.send, data))
+    result = yield From(delay(loop, socket.recv))
+    socket.close()  # TODO: LRU sockets
+    result = loads(result)
+    raise Return(result)
+
+
+def keys_to_data(o, data):
+    """
+
+    >>> keys_to_data(('x', 'y'), {'x': 1})
+    (1, 'y')
+    >>> keys_to_data({'a': 'x', 'b': 'y'}, {'x': 1})
+    {'a': 1, 'b': 'y'}
+    """
+    if isinstance(o, (tuple, list)):
+        result = []
+        for arg in o:
+            try:
+                result.append(data[arg])
+            except (TypeError, KeyError):
+                result.append(arg)
+        result = type(o)(result)
+
+    if isinstance(o, dict):
+        result = {}
+        for k, v in o.items():
+            try:
+                result[k] = data[v]
+            except (TypeError, KeyError):
+                result[k] = v
+    return result
