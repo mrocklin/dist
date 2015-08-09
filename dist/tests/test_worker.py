a5dcb1595b021c50ffed96edc16c12db4e18d02e
Matthew Rocklin <mrocklin@gmail.com>
2015-08-08 21:28:46 -0700

    test worker

--- dist/tests/test_worker.py
+++ dist/tests/test_worker.py
@@ -1,8 +1,50 @@
-from dist.worker import Worker
+from dist import Worker, MDStore
+from dist.worker import loads, dumps
 import trollius as asyncio
+from contextlib import contextmanager
+import zmq
+
+
+context = zmq.Context()
+
+
+@contextmanager
+def dealer(addr):
+    socket = context.socket(zmq.DEALER)
+    socket.connect(addr)
+    try:
+        yield socket
+    finally:
+        socket.close()
+
+
+@contextmanager
+def mdstore():
+    mds = MDStore('*', 8008)
+    mds.start()
+
+    try:
+        yield mds
+    finally:
+        mds.close()
+
+
+@contextmanager
+def worker(metadata_addr):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    w = Worker('127.0.0.1', 3483, '*', metadata_addr, loop=loop)
+    w.start()
+
+    try:
+        yield w
+    finally:
+        w.close()
 
 def test_Worker():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    w = Worker('tcp://127.0.0.1:8000', 'tcp://127.0.0.1:8001',
-               None, loop=loop)
-    w.start()
+    with mdstore() as mds:
+        with worker(metadata_addr='tcp://127.0.0.1:%d' % mds.port) as w:
+            with dealer(w.address) as sock:
+                msg = {'op': 'ping'}
+                sock.send(dumps(msg))
+                result = sock.recv()
+                assert result == b'pong'
--- dist/worker.py
+++ dist/worker.py
@@ -1,28 +1,25 @@
 import trollius as asyncio
 from trollius import From, Return
-from concurrent.futures import ThreadPoolExecutor
 from toolz import merge, get
 from dill import dumps, loads
+from threading import Thread
+from .utils import delay
 import zmq
 
-executor = ThreadPoolExecutor(1)
 context = zmq.Context()
-
-
-def delay(loop, func, *args, **kwargs):
-    """ Run function in separate thread, turn into coroutine """
-    future = executor.submit(func, *args2, **kwargs2)
-    return asyncio.futures.wrap_future(future, loop=loop)
 
 
 @asyncio.coroutine
 def dealer_send_recv(loop, addr, data):
     socket = context.socket(zmq.DEALER)
     socket.connect(addr)
+    if not isinstance(data, bytes):
+        data = dumps(data)
     yield From(delay(loop, socket.send, data))
-    data = yield From(delay(loop, socket.recv))
+    result = yield From(delay(loop, socket.recv))
     socket.close()  # TODO: LRU sockets
-    raise Return(data)
+    result = loads(result)
+    raise Return(result)
 
 
 def keys_to_data(o, data):
@@ -46,14 +43,14 @@ def keys_to_data(o, data):
 
 
 @asyncio.coroutine
-def get_data(keys, local_data, metadata_addr, update=False):
+def get_data(loop, keys, local_data, metadata_addr, update=False):
     local = {k: local_data[k] for k in needed if k in local_data},
     missing = [k for k in needed if k not in local_data]
 
-    msg = {'op': 'who-has', 'data': other}
-    who_has = yield From(dealer_send_recv(metadata_attr, msg))
+    msg = {'op': 'who-has', 'keys': other}
+    who_has = yield From(dealer_send_recv(loop, metadata_attr, msg))
 
-    coroutines = [dealer_send_recv(random.choice(who_has[k]),
+    coroutines = [dealer_send_recv(loop, random.choice(who_has[k]),
                                    {'op': 'get-data', 'keys': [k]})
                                 for k in missing]
 
@@ -73,7 +70,7 @@ def compute(loop, msg, local_data, metadata_addr, store=True):
         key, func, args, kwargs, needed = \
                 get(msg, 'key', 'func', 'args', 'kwargs', 'needed')
 
-        data = yield From(get_data(keys, local_data, metadata_addr))
+        data = yield From(get_data(loop, keys, local_data, metadata_addr))
 
         args2 = keys_to_data(args, data)
         kwargs2 = keys_to_data(kwargs, data)
@@ -87,61 +84,107 @@ def compute(loop, msg, local_data, metadata_addr, store=True):
 
 
 class Worker(object):
-    def __init__(self, address, bind, metadata_addr, loop=None):
-        self.router = context.socket(zmq.ROUTER)
-        self.router.bind(bind)
+    def __init__(self, ip, port, bind_ip,
+                 metadata_addr, loop=None):
+        self.ip = ip
+        self.port = port
+        self.bind_ip = bind_ip
         self.metadata_addr = metadata_addr
         self.data = dict()
         self.loop = loop or asyncio.get_event_loop()
         self.work_q = asyncio.Queue(loop=self.loop)
         self.send_q = asyncio.Queue(loop=self.loop)
 
+    @property
+    def address(self):
+        return 'tcp://%s:%d' % (self.ip, self.port)
 
     @asyncio.coroutine
     def listen(self):
         while True:
-            addr, bytes = yield From(delay(self.loop, self.router.recv_multipart))
+            try:
+                socks = yield From(delay(self.loop, self.poller.poll))
+                socks = dict(socks)
+            except KeyboardInterrupt:
+                break
+            print("Communication received")
+            if self._local_router in socks:
+                addr, bytes = self._local_router.recv_multipart()
+            elif self.router in socks:
+                result = yield From(delay(self.loop, self.router.recv_multipart))
+                addr, bytes = result
+
             msg = loads(bytes)
-            if msg['op'] == 'close':
+            print("msg: %s" % str(msg))
+            if msg == 'close':
                 break
             if msg['op'] == 'compute':
-                work_q.put(msg)
+                work_q.put_nowait(msg)
             if msg['op'] == 'get-data':
                 data = {k: self.data[k] for k in msg['keys']
                                          if k in self.data}
-                send_q.put((addr, data))
+                self.send_q.put_nowait((addr, data))
             if msg['op'] == 'ping':
-                send_q.put((addr, b'pong'))
+                self.send_q.put_nowait((addr, b'pong'))
+            else:
+                raise NotImplementedError("Bad Message: %s" % msg)
 
     @asyncio.coroutine
     def compute(self):
         while True:
-            addr, msg = self.work_q.get()
-            if msg['op'] == 'close':
+            addr, msg = yield From(self.work_q.get())
+            print('compute', msg)
+            if msg == 'close':
                 break
 
             result = yield From(compute(self.loop, msg, self.data, self.metadata_addr))
             out = {'op': 'computation-finished',
                    'key': msg['key']}
-            self.send_q.put((addr, out))
+            self.send_q.put_nowait((addr, out))
 
     @asyncio.coroutine
     def reply(self):
         while True:
-            addr, msg = self.send_q.get()
-            if msg['op'] == 'close':
+            addr, msg = yield From(self.send_q.get())
+            print('reply', msg)
+            if msg == 'close':
                 break
             if not isinstance(msg, bytes):
                 msg = dumps(msg)
             yield From(delay(self.loop, self.router.send_multipart, [addr, msg]))
 
     def close(self):
-        self.send_q.put((None, 'close'))
-        self.work_q.put((None, 'close'))
-        self.listen_q.put((None, 'close'))
-        self.router.close(linger=1)
+        self._local_dealer.send(dumps('close'))
+        self.send_q.put_nowait((None, 'close'))
+        self.work_q.put_nowait((None, 'close'))
+        self._thread.join()
+        self.router.close()
+        self._local_router.close()
+        self._local_dealer.close()
 
-    def run(self):
-        self.loop.run_until_complete(asyncio.gather(self.listen(),
-                                                    self.compute(),
-                                                    self.reply()))
+    def start(self, thread=True):
+        self.router = context.socket(zmq.ROUTER)
+        self.router.bind('tcp://%s:%d' % (self.bind_ip, self.port))
+        print("Binding router to %s" % 'tcp://%s:%d' % (self.bind_ip,
+            self.port))
+
+        self._local_router = context.socket(zmq.ROUTER)
+        port = self._local_router.bind_to_random_port('tcp://127.0.0.1')
+        self._local_dealer = context.socket(zmq.DEALER)
+        self._local_dealer.connect('tcp://127.0.0.1:%d' % port)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.router, zmq.POLLIN)
+        self.poller.register(self._local_router, zmq.POLLIN)
+
+        cor = asyncio.gather(*[asyncio.Task(cor, loop=self.loop)
+                               for cor in [self.listen(),
+                                           self.compute(),
+                                           self.reply()]])
+
+        if thread:
+            self._thread = Thread(target=self.loop.run_until_complete,
+                                  args=(cor,))
+            self._thread.start()
+        else:
+            self.loop.run_until_complete(cor)
